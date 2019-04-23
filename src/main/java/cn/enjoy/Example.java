dd15737678046e06eecf55d0090b598782197d6d
jiahongwen <1023951359@qq.com>
2019-04-23 16:14:15 +0800

    new git

---
+++ src/main/java/cn/enjoy/Example.java
@@ -0,0 +1,20 @@
+package cn.enjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+    String home(){
+        return "Hello Git";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class, args);
+    }
+}
